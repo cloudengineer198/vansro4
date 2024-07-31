fe0bbd7f6bdcdf999d636895c10a6417ede3014b
kk <kk@gmail.com>
2024-07-31 09:36:31 +0000

    first commit

---
+++ Sample.java
@@ -0,0 +1,10 @@
+public class Sample
+{
+public static void main(String args[])
+    {
+System.out.println("Vansro Technologies");
+System.out.println("Student Login Page");
+System.out.println("Enter Your User Id:");
+System.out.println("Enter Your Password");
+  }
+}
